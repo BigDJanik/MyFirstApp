8a1e0456bfe8648d8a22349b9cb2f9efdce0f533
janik <76305@hs-aalen.de>
2023-12-03 19:45:39 +0100

    feature: Level 3: Data and info Notes

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 import json
 import pandas as pd
 
@@ -34,3 +35,38 @@ async def get_stats(team_type:str="team"):
     stats.index += 1
 
     return stats.to_json(orient="index")
+
+@app.get("/level-3/wins")
+async def get_info():
+    raw_data = FileHandler()
+    raw_data_df = pd.DataFrame(raw_data["games"])
+
+    condition_1 = raw_data_df["points_scored"] - raw_data_df["points_allowed"] > 3
+    condition_2 = raw_data_df["points_scored"] - raw_data_df["points_allowed"] < 0
+
+    prepared_data_df = raw_data_df.copy()
+    prepared_data_df["true wins"] = condition_1 | condition_2
+
+    return prepared_data_df.to_json(orient="index")
+
+@app.get("/level-3/info_notes")
+async def get_info():
+    info_notes = """
+                    **Heimvorteil sind +3 Punkte im Handicap**  
+                    *(Modellierte Annahme)*
+                    
+                    ---
+
+                    Erklärung:
+            
+                    Die Schätzung des Heimvorteils in der NFL auf ungefähr 2,5 bis 3 Punkte pro Spiel basiert auf einer Kombination von historischen Daten, Studien und Erfahrungen von Sportanalysten. Es ist wichtig zu beachten, dass dies eine allgemeine Schätzung ist und keine exakte wissenschaftliche Berechnung darstellt. Hier sind einige der Quellen und Grundlagen, auf denen diese Schätzung basiert:
+
+                    - **Historische Daten:** Durch die Analyse von jahrzehntelangen NFL-Spielprotokollen können Sportanalysten Muster erkennen, die darauf hinweisen, dass Teams, die zu Hause spielen, tendenziell bessere Ergebnisse erzielen als bei Auswärtsspielen. Dies kann als Ausgangspunkt für die Schätzung des Heimvorteils dienen.
+
+                    - **Akademische Studien:** Es gibt einige akademische Studien und wissenschaftliche Arbeiten, die den Heimvorteil im Sport, einschließlich der NFL, untersuchen. Diese Studien nutzen statistische Methoden, um den Heimvorteil zu quantifizieren. Obwohl die Ergebnisse variieren können, zeigen viele dieser Studien einen Heimvorteil von etwa 2,5 bis 3 Punkten pro Spiel.
+
+                    - **Erfahrung von Sportanalysten:** Sportexperten und Analysten, die die NFL und andere Sportligen abdecken, bringen ihre Erfahrung und Einsichten in die Schätzung des Heimvorteils ein. Dies kann auf beobachteten Mustern und ihrer Kenntnis der Dynamik von Heim- und Auswärtsspielen basieren.
+                    """
+
+
+    return HTMLResponse(content=info_notes)
--- frontend/app.py
+++ frontend/app.py
@@ -4,12 +4,15 @@ import streamlit as st
 import json
 import pandas as pd
 
-URL = "http://172.17.0.1:8000"
-
+# Fürs containern ändern:
+#URL = "http://172.17.0.1:8000"
+URL = "http://127.0.0.1:8000"
 
 ENDPOINT_DATA = URL + "/level-1/data"
 ENDPOINT_TEAMS = URL + "/level-1/teams"
 ENDPOINT_STATS = URL + "/level-2/stats"
+ENDPOINT_WINS = URL + "/level-3/wins"
+ENDPOINT_INFO = URL + "/level-3/info_notes"
 
 
 def provide_raw_data():
@@ -19,13 +22,9 @@ def provide_raw_data():
 
     with st.expander(label="Raw Data"):
         st.json(raw_data)
-
     return
 
 def provide_derived_data():
-
-
-
     with st.expander(label="Insights"):
         
         team_types = ["team", "opponent"]
@@ -43,35 +42,17 @@ def provide_derived_data():
             st.write(df)
     return
 
-def provide_algorithm(raw_data_df):
+def provide_algorithm():
     with st.expander("Algorithm for Home Advantage"):
         st.markdown(
-            """
-                    **Heimvorteil sind +3 Punkte im Handicap**  
-                    *(Modellierte Annahme)*
-                    
-                    ---
-
-                    Erklärung:
-            
-                    Die Schätzung des Heimvorteils in der NFL auf ungefähr 2,5 bis 3 Punkte pro Spiel basiert auf einer Kombination von historischen Daten, Studien und Erfahrungen von Sportanalysten. Es ist wichtig zu beachten, dass dies eine allgemeine Schätzung ist und keine exakte wissenschaftliche Berechnung darstellt. Hier sind einige der Quellen und Grundlagen, auf denen diese Schätzung basiert:
-
-                    - **Historische Daten:** Durch die Analyse von jahrzehntelangen NFL-Spielprotokollen können Sportanalysten Muster erkennen, die darauf hinweisen, dass Teams, die zu Hause spielen, tendenziell bessere Ergebnisse erzielen als bei Auswärtsspielen. Dies kann als Ausgangspunkt für die Schätzung des Heimvorteils dienen.
-
-                    - **Akademische Studien:** Es gibt einige akademische Studien und wissenschaftliche Arbeiten, die den Heimvorteil im Sport, einschließlich der NFL, untersuchen. Diese Studien nutzen statistische Methoden, um den Heimvorteil zu quantifizieren. Obwohl die Ergebnisse variieren können, zeigen viele dieser Studien einen Heimvorteil von etwa 2,5 bis 3 Punkten pro Spiel.
-
-                    - **Erfahrung von Sportanalysten:** Sportexperten und Analysten, die die NFL und andere Sportligen abdecken, bringen ihre Erfahrung und Einsichten in die Schätzung des Heimvorteils ein. Dies kann auf beobachteten Mustern und ihrer Kenntnis der Dynamik von Heim- und Auswärtsspielen basieren.
-                    """
+            # info_notes
+            requests.get(url=ENDPOINT_INFO).text        # Abrufen der Info Notes
         )
 
-        prepared_data_df = raw_data_df.copy()
+        data = requests.get(url=ENDPOINT_WINS).json()
+        prepared_data = pd.read_json(StringIO(data), orient="index")
 
-        condition_1 = raw_data_df["points_scored"] - raw_data_df["points_allowed"] > 3
-        condition_2 = raw_data_df["points_scored"] - raw_data_df["points_allowed"] < 0
-
-        prepared_data_df["true wins"] = condition_1 | condition_2
-
-        st.write(prepared_data_df)
+        st.write(prepared_data)
     return
 
 def provide_decision_support(home_stats, away_stats, home_team, away_team):
@@ -142,8 +123,8 @@ def main():
     # Level 2
     provide_derived_data()
 
-    # # Level 3
-    # provide_algorithm(raw_data_df=raw_data_df)
+    # Level 3
+    provide_algorithm()
 
     # # Level 4
     # (
